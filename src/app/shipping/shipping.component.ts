f432a3e90feb490cf47a241f9883b705be52e08a
oaf-edwin <82295641+oaf-edwin@users.noreply.github.com>
2023-07-01 17:50:51 -0700

    3 files modified

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-shipping',
@@ -6,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./shipping.component.css']
 })
 export class ShippingComponent {
+  constructor(private cartService: CartService) {}
+  shippingCosts!: Observable<{type:string, price:number}[]>;
 
+  ngOnInit(): void {
+    this.shippingCosts = this.cartService.getShippingPrices();
+  }
 }
